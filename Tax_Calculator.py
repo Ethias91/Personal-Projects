bcd44762e01790b8d8fecdb1910ac28bc2f448c0
Ethias91 <165411061+Ethias91@users.noreply.github.com>
2024-06-22 11:03:39 -0500

    On branch main
    Initial commit
    
    Changes to be committed:
    	new file:   Tax_Calculator.py
    
    Changes not staged for commit:
    	modified:   Tax_Calculator.py
    
     Untracked files:
    	readme.txt

---
+++ Tax_Calculator.py
@@ -0,0 +1,14 @@
+# Let's try building a program that will let me calculate taxes on simple purchases. right?
+cost = input("What is the subtotal of the product? $")
+# Updated to ask what tax rate instead
+Tax = input("What is the tax rate of your state? Enter in the format [0.00] $")
+Tax_rate = float(Tax) / 100
+tax_total = float(cost) * float(Tax_rate)
+# I had to look this up
+rounded_tax = round(tax_total, 2)
+total = float(cost) + float(tax_total)
+rounded_number = round(total, 2)
+print("Taxes amount: ")
+print(rounded_tax)
+print("and your total comes out to")
+print(rounded_number)
