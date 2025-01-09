f902444c6cd80fff21e1279a48d26a2e0a2b6b8f
Ethias91 <165411061+Ethias91@users.noreply.github.com>
2025-01-09 16:33:14 -0600

    Added comments to explain parts of the code

--- Number_guessing_game.py
+++ Number_guessing_game.py
@@ -2,6 +2,7 @@
 import random
 print("Lets play a guessing game. Choose a number between 1 and 10")
 Number = random.randint(1,10)
+#This line sets up the Guess variable to be maleable in the While If Else loop
 Guess = None
 while Guess != Number:
     Guess = int(input("What is your guess? :"))
@@ -11,3 +12,4 @@ while Guess != Number:
         break
     else:
         print("Try again")
+#Once the loop is completed the code breaks and ends the game.
